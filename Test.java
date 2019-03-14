b3e018f45190d464923609d76925da0ad244922c
Travis M. Slattery <Travis.M.Slattery@students.ittralee.ie>
2019-03-14 14:46:57 +0000

    adding the java file

---
+++ Test.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)Test.java
+ *
+ *
+ * @author 
+ * @version 1.00 2019/3/14
+ */
+
+
+public class Test {
+
+    public static void main(String args[]) {
+    	
+    	System.out.println("Hello test repo");
+    }
+    
+    
+}
